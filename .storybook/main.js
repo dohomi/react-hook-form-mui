1a215c47fb21ac99012dbe4ab6dcf478506268c1
Dominic Garms <djgarms@gmail.com>
2020-11-07 12:14:39 +0800

    change to lazy

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,24 +1,4 @@
 module.exports = {
   stories: ['../stories/**/*.stories.@(tsx)'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-docs', '@storybook/addon-knobs'],
-  // webpackFinal: async (config) => {
-  //   config.module.rules.push({
-  //     test: /\.(ts|tsx)$/,
-  //     use: [
-  //       {
-  //         loader: require.resolve('ts-loader'),
-  //         options: {
-  //           transpileOnly: true
-  //         }
-  //       },
-  //       {
-  //         loader: require.resolve('react-docgen-typescript-loader')
-  //       }
-  //     ]
-  //   })
-  //
-  //   config.resolve.extensions.push('.ts', '.tsx')
-  //
-  //   return config
-  // }
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-docs', '@storybook/addon-knobs']
 }
