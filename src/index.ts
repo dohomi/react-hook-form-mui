c788fab31a66c982a7befe117da28b6de2a3c19c
Dominic Garms <djgarms@gmail.com>
2020-11-12 15:34:05 +0800

    fix types

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
 import universal from 'react-universal-component'
+import { FC } from 'react'
+import {
+  CheckboxButtonGroupProps,
+  CheckboxElementProps,
+  DatePickerElementProps,
+  FormContainerProps,
+  MultiSelectElementProps,
+  PasswordElementProps,
+  RadioButtonGroupProps,
+  SelectElementProps,
+  TextFieldElementProps
+} from './formTypes'
 
-const TextFieldElement = universal(() => import('./TextFieldElement'))
-const FormContainer = universal(() => import('./FormContainer'))
-const RadioButtonGroup = universal(() => import('./RadioButtonGroup'))
-const CheckboxButtonGroup = universal(() => import('./CheckboxButtonGroup'))
-const CheckboxElement = universal(() => import('./CheckboxElement'))
-const PasswordElement = universal(() => import('./PasswordElement'))
-const SelectElement = universal(() => import('./SelectElement'))
-const MultiSelectElement = universal(() => import('./MultiSelectElement'))
-const DatePickerElement = universal(() => import('./DatePickerElement'))
+const TextFieldElement = universal(import('./TextFieldElement')) as FC<TextFieldElementProps>
+const FormContainer = universal(import('./FormContainer')) as FC<FormContainerProps>
+const RadioButtonGroup = universal(import('./RadioButtonGroup')) as FC<RadioButtonGroupProps>
+const CheckboxButtonGroup = universal(import('./CheckboxButtonGroup')) as FC<CheckboxButtonGroupProps>
+const CheckboxElement = universal(import('./CheckboxElement')) as FC<CheckboxElementProps>
+const PasswordElement = universal(import('./PasswordElement')) as FC<PasswordElementProps>
+const SelectElement = universal(import('./SelectElement')) as FC<SelectElementProps>
+const MultiSelectElement = universal(import('./MultiSelectElement')) as FC<MultiSelectElementProps>
+const DatePickerElement = universal(import('./DatePickerElement')) as FC<DatePickerElementProps>
 
 export {
   TextFieldElement,
