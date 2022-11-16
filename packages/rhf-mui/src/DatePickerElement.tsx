e273bc9d6e0f0a6a0730d2d15c06bab92587136f
Dominic Garms <djgarms@gmail.com>
2022-11-16 17:08:42 +0800

    improve keyboard input

--- packages/rhf-mui/src/DatePickerElement.tsx
+++ packages/rhf-mui/src/DatePickerElement.tsx
@@ -28,76 +28,77 @@ export type DatePickerElementProps<T extends FieldValues, TInputDate, TDate = TI
 }
 
 export default function DatePickerElement<TFieldValues extends FieldValues>({
-                                                                                isDate,
-                                                                                parseError,
-                                                                                name,
-                                                                                required,
-                                                                                parseDate,
-                                                                                validation = {},
-                                                                                inputProps,
-                                                                                control,
-                                                                                ...rest
-                                                                            }: DatePickerElementProps<TFieldValues, any, any>): JSX.Element {
+  isDate,
+  parseError,
+  name,
+  required,
+  parseDate,
+  validation = {},
+  inputProps,
+  control,
+  ...rest
+}: DatePickerElementProps<TFieldValues, any, any>): JSX.Element {
 
-    if (required && !validation.required) {
-        validation.required = 'This field is required'
-    }
+  if (required && !validation.required) {
+    validation.required = 'This field is required'
+  }
 
-    return (
-        <Controller
-            name={name}
-            rules={validation}
-            control={control}
-            render={({
-                         field: {onChange, value},
-                         fieldState: {error, invalid}
-                     }) => (
-                <DatePicker
-                    {...rest}
-                    value={value || ''}
-                    onChange={(value, keyboardInputValue) => {
-                        let newValue: undefined | string = undefined
-                        if (keyboardInputValue) {
-                            if (typeof parseDate === 'function') {
-                                newValue = parseDate(value, keyboardInputValue)
-                            } else {
-                                newValue = value
-                            }
-                        } else {
-                            if (typeof parseDate === 'function') {
-                                newValue = parseDate(value)
-                            } else {
-                                newValue = value
-                            }
-                        }
-                        onChange(newValue, keyboardInputValue)
-                        if (typeof rest.onChange === 'function') {
-                            rest.onChange(newValue, keyboardInputValue)
-                        }
-                    }}
-                    renderInput={(params) => (
-                        <TextField
-                            {...params}
-                            inputProps={{
-                                ...params?.inputProps,
-                                ...(!value && {
-                                    value: ''
-                                })
-                            }}
-                            {...inputProps}
-                            required={!!required}
-                            error={invalid}
-                            helperText={
-                                error
-                                    ? typeof parseError === 'function'
-                                        ? parseError(error)
-                                        : error.message
-                                    : inputProps?.helperText || rest.helperText
-                            }
-                        />
-                    )}
-                />
-            )}
+  return (
+    <Controller
+      name={name}
+      rules={validation}
+      control={control}
+      render={({
+        field: {onChange, value},
+        fieldState: {error, invalid}
+      }) => (
+        <DatePicker
+          {...rest}
+          value={value || ''}
+          onChange={(value, keyboardInputValue) => {
+            let newValue: undefined | string = undefined
+            if (keyboardInputValue) {
+              if (typeof parseDate === 'function') {
+                newValue = parseDate(value, keyboardInputValue)
+              } else {
+                newValue = value
+              }
+            } else {
+              if (typeof parseDate === 'function') {
+                newValue = parseDate(value)
+              } else {
+                newValue = value
+              }
+            }
+
+            onChange(newValue, keyboardInputValue)
+            if (typeof rest.onChange === 'function') {
+              rest.onChange(newValue, keyboardInputValue)
+            }
+          }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              inputProps={{
+                ...params?.inputProps,
+                ...(!value && {
+                  value: ''
+                })
+              }}
+              {...inputProps}
+              required={!!required}
+              error={invalid}
+              helperText={
+                error
+                  ? typeof parseError === 'function'
+                    ? parseError(error)
+                    : error.message
+                  : inputProps?.helperText || rest.helperText
+              }
+            />
+          )}
         />
-    )
+      )}
+    />
+  )
 }
--- packages/rhf-mui/src/DateTimePickerElement.tsx
+++ packages/rhf-mui/src/DateTimePickerElement.tsx
@@ -28,76 +28,76 @@ export type DateTimePickerElementProps<T extends FieldValues, TInputDate, TDate 
 }
 
 export default function DateTimePickerElement<TFieldValues extends FieldValues>({
-                                                                                    isDate,
-                                                                                    parseError,
-                                                                                    name,
-                                                                                    required,
-                                                                                    parseDate,
-                                                                                    validation = {},
-                                                                                    inputProps,
-                                                                                    control,
-                                                                                    ...rest
-                                                                                }: DateTimePickerElementProps<TFieldValues, any, any>): JSX.Element {
+  isDate,
+  parseError,
+  name,
+  required,
+  parseDate,
+  validation = {},
+  inputProps,
+  control,
+  ...rest
+}: DateTimePickerElementProps<TFieldValues, any, any>): JSX.Element {
 
-    if (required && !validation.required) {
-        validation.required = 'This field is required'
-    }
+  if (required && !validation.required) {
+    validation.required = 'This field is required'
+  }
 
-    return (
-        <Controller
-            name={name}
-            rules={validation}
-            control={control}
-            render={({
-                         field: {onChange, value},
-                         fieldState: {error, invalid}
-                     }) => (
-                <DateTimePicker
-                    {...rest}
-                    value={value || ''}
-                    onChange={(value, keyboardInputValue) => {
-                        let newValue: string | undefined = undefined
-                        if (keyboardInputValue) {
-                            if (typeof parseDate === 'function') {
-                                newValue = parseDate(value, keyboardInputValue)
-                            } else {
-                                newValue = keyboardInputValue
-                            }
-                        } else {
-                            if (typeof parseDate === 'function') {
-                                newValue = parseDate(value)
-                            } else {
-                                newValue = value
-                            }
-                        }
-                        onChange(newValue, keyboardInputValue)
-                        if (typeof rest.onChange === 'function') {
-                            rest.onChange(newValue, keyboardInputValue)
-                        }
-                    }}
-                    renderInput={(params) => (
-                        <TextField
-                            {...params}
-                            inputProps={{
-                                ...params?.inputProps,
-                                ...(!value && {
-                                    value: ''
-                                })
-                            }}
-                            {...inputProps}
-                            required={!!required}
-                            error={invalid}
-                            helperText={
-                                error
-                                    ? typeof parseError === 'function'
-                                        ? parseError(error)
-                                        : error.message
-                                    : inputProps?.helperText || rest.helperText
-                            }
-                        />
-                    )}
-                />
-            )}
+  return (
+    <Controller
+      name={name}
+      rules={validation}
+      control={control}
+      render={({
+        field: {onChange, value},
+        fieldState: {error, invalid}
+      }) => (
+        <DateTimePicker
+          {...rest}
+          value={value || ''}
+          onChange={(value, keyboardInputValue) => {
+            let newValue: string | undefined = undefined
+            if (keyboardInputValue) {
+              if (typeof parseDate === 'function') {
+                newValue = parseDate(value, keyboardInputValue)
+              } else {
+                newValue = value
+              }
+            } else {
+              if (typeof parseDate === 'function') {
+                newValue = parseDate(value)
+              } else {
+                newValue = value
+              }
+            }
+            onChange(newValue, keyboardInputValue)
+            if (typeof rest.onChange === 'function') {
+              rest.onChange(newValue, keyboardInputValue)
+            }
+          }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              inputProps={{
+                ...params?.inputProps,
+                ...(!value && {
+                  value: ''
+                })
+              }}
+              {...inputProps}
+              required={!!required}
+              error={invalid}
+              helperText={
+                error
+                  ? typeof parseError === 'function'
+                    ? parseError(error)
+                    : error.message
+                  : inputProps?.helperText || rest.helperText
+              }
+            />
+          )}
         />
-    )
+      )}
+    />
+  )
 }
