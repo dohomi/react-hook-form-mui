39a5f23bf88f51babdb7efb2b75f3fed8ceaa238
Dominic Garms <djgarms@gmail.com>
2022-11-16 10:03:21 +0800

    change storybook and update all deps

--- apps/nextjs/src/components/Layout.tsx
+++ apps/nextjs/src/components/Layout.tsx
@@ -1,29 +1,29 @@
-import {FunctionComponent, PropsWithChildren} from "react";
-import {AppBar, Box, Button, Toolbar} from "@mui/material";
-import Link from "next/link";
+import {FunctionComponent, PropsWithChildren} from 'react'
+import {AppBar, Box, Button, Toolbar} from '@mui/material'
+import Link from 'next/link'
 
 export const Layout: FunctionComponent<PropsWithChildren> = ({children}) => {
-    return (
-        <Box>
-            <AppBar>
-                <Toolbar>
-                    <Link href={'/'} passHref>
-                        <Button color={'inherit'}>
+  return (
+    <Box>
+      <AppBar>
+        <Toolbar>
+          <Link href={'/'} passHref legacyBehavior>
+            <Button color={'inherit'}>
                             Base
-                        </Button>
-                    </Link>
-                    <Link href={'/withSub'} passHref>
-                        <Button color={'inherit'}>
+            </Button>
+          </Link>
+          <Link href={'/withSub'} passHref legacyBehavior>
+            <Button color={'inherit'}>
                             With useWatch
-                        </Button>
-                    </Link>
-                </Toolbar>
-            </AppBar>
-            <Toolbar/>
-            <Box padding={3}>
+            </Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+      <Toolbar/>
+      <Box padding={3}>
 
-                {children}
-            </Box>
-        </Box>
-    )
+        {children}
+      </Box>
+    </Box>
+  )
 }
--- apps/nextjs/src/pages/withSub.tsx
+++ apps/nextjs/src/pages/withSub.tsx
@@ -1,43 +1,50 @@
-import {Button} from "@mui/material";
-import {FormContainer, TextFieldElement, useWatch} from "react-hook-form-mui";
+import {Alert, Button, Stack} from '@mui/material'
+import {FormContainer, TextFieldElement, useWatch} from 'react-hook-form-mui'
 
 const SubComponent = () => {
-    const [name, email] = useWatch({
-        name: ["name", "email"]
-    });
-    console.log(name, email);
-    return (
-        <>
-            <Button type={"submit"} color={"primary"} disabled={!(name && email)}>
-                Submit
-            </Button>
-        </>
-    );
-};
+  const [name, email] = useWatch({
+    name: ['name', 'email']
+  })
+  return (
+    <>
+      <Stack spacing={3}>
+        <Button type={'submit'} color={'primary'}
+          variant={'contained'}
+          disabled={!(name && email)}>
+                    Submit
+        </Button>
+        <Alert variant={'outlined'} severity={'info'}>You have to fill out the required fields before the Button
+                    activates.</Alert>
+      </Stack>
+    </>
+  )
+}
 
 export default function IndexPage() {
 
-    return (
-        <div>
-            <FormContainer
-                defaultValues={{
-                    name: ""
-                }}
-                onSuccess={(data) => {
-                    console.log(data)
-                }}
-            >
-                <TextFieldElement name={"name"} label={"Name"} required/> <br/>
-                <TextFieldElement
-                    name={"email"}
-                    label={"Email"}
-                    required
-                    type={"email"}
-                />{" "}
-                <br/>
-                <TextFieldElement name={"interest"} label={"Interest"}/> <br/>
-                <SubComponent/>
-            </FormContainer>
-        </div>
-    );
+  return (
+    <div>
+      <FormContainer
+        defaultValues={{
+          name: ''
+        }}
+        onSuccess={(data) => {
+          console.log(data)
+        }}
+      >
+        <Stack direction={'column'}>
+          <TextFieldElement name={'name'} label={'Name'} required/> <br/>
+          <TextFieldElement
+            name={'email'}
+            label={'Email'}
+            required
+            type={'email'}
+          />{' '}
+          <br/>
+          <TextFieldElement name={'interest'} label={'Interest'}/> <br/>
+          <SubComponent/>
+        </Stack>
+      </FormContainer>
+    </div>
+  )
 }
