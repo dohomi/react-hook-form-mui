090679bae6960ecceaacc49a880d64b66f7cc8ca
Alexander Rubtsov <arubtsov@fxcompared.com>
2022-12-13 15:45:10 +0000

    element should have a focus ref

--- packages/rhf-mui/src/TimePickerElement.tsx
+++ packages/rhf-mui/src/TimePickerElement.tsx
@@ -57,7 +57,7 @@ export default function TimePickerElement<TFieldValues extends FieldValues>({
       rules={validation}
       control={control}
       render={({
-        field: { onChange, value },
+        field: { onChange, value, ref },
         fieldState: { error, invalid },
       }) => (
         <TimePicker
@@ -102,6 +102,7 @@ export default function TimePickerElement<TFieldValues extends FieldValues>({
                     : error.message
                   : inputProps?.helperText || rest.helperText
               }
+              inputRef={ref}
             />
           )}
         />
