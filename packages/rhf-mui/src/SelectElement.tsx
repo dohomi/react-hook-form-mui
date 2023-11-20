d9622a41cc24a9c45ab1070e753b5a1aa44180b0
sadik-malik <sadik.malik@vietpay.com>
2023-11-20 18:00:03 +0530

    fix: allow ref and inputRef prop for SelectElement

--- packages/rhf-mui/src/SelectElement.tsx
+++ packages/rhf-mui/src/SelectElement.tsx
@@ -1,21 +1,21 @@
-import {ReactNode, createElement} from 'react'
-import {MenuItem, TextField, TextFieldProps} from '@mui/material'
+import {ReactNode, createElement, forwardRef, Ref, RefAttributes} from 'react'
+import {MenuItem, TextField, TextFieldProps, useForkRef} from '@mui/material'
 import {
   Control,
-  Controller,
-  ControllerProps,
   FieldError,
-  Path,
+  FieldValues,
+  FieldPath,
+  UseControllerProps,
+  useController,
 } from 'react-hook-form'
-import {FieldValues} from 'react-hook-form/dist/types/fields'
 import {useFormError} from './FormErrorProvider'
 
-export type SelectElementProps<T extends FieldValues> = Omit<
-  TextFieldProps,
-  'name' | 'type' | 'onChange'
-> & {
-  validation?: ControllerProps<T>['rules']
-  name: Path<T>
+export type SelectElementProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+> = Omit<TextFieldProps, 'name' | 'type' | 'onChange'> & {
+  validation?: UseControllerProps<TFieldValues, TName>['rules']
+  name: TName
   options?:
     | readonly {
         id: string | number
@@ -29,94 +29,116 @@ export type SelectElementProps<T extends FieldValues> = Omit<
   parseError?: (error: FieldError) => ReactNode
   objectOnChange?: boolean
   onChange?: (value: any) => void
-  control?: Control<T>
+  control?: Control<TFieldValues>
 }
 
-export default function SelectElement<TFieldValues extends FieldValues>({
-  name,
-  required,
-  valueKey = 'id',
-  labelKey = 'label',
-  options = [],
-  parseError,
-  type,
-  objectOnChange,
-  validation = {},
-  control,
-  ...rest
-}: SelectElementProps<TFieldValues>): JSX.Element {
+type SelectElementComponent = <
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+>(
+  props: SelectElementProps<TFieldValues, TName> & RefAttributes<HTMLDivElement>
+) => JSX.Element
+
+const SelectElement = forwardRef(function SelectElement<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+>(
+  props: SelectElementProps<TFieldValues, TName>,
+  ref: Ref<HTMLDivElement>
+): JSX.Element {
+  const {
+    name,
+    required,
+    valueKey = 'id',
+    labelKey = 'label',
+    options = [],
+    parseError,
+    type,
+    objectOnChange,
+    validation = {},
+    control,
+    inputRef,
+    ...rest
+  } = props
+
   const errorMsgFn = useFormError()
   const customErrorFn = parseError || errorMsgFn
   const isNativeSelect = !!rest.SelectProps?.native
   const ChildComponent = isNativeSelect ? 'option' : MenuItem
 
-  if (required && !validation.required) {
-    validation.required = 'This field is required'
+  const rules = {
+    ...validation,
+    ...(required &&
+      !validation.required && {
+      required: 'This field is required',
+    }),
   }
 
+  const {
+    field,
+    fieldState: {error},
+  } = useController({
+    name,
+    rules,
+    control,
+  })
+
+  const handleInputRef = useForkRef(field.ref, inputRef)
+
+  // handle shrink on number input fields
+  if (type === 'number' && typeof field.value !== 'undefined') {
+    rest.InputLabelProps = rest.InputLabelProps || {}
+    rest.InputLabelProps.shrink = true
+  }
+
+  const currentValue = field.value?.[valueKey] ?? field.value // try fetch key value
+
   return (
-    <Controller
+    <TextField
+      {...rest}
       name={name}
-      rules={validation}
-      control={control}
-      render={({
-        field: {onBlur, onChange, value, ref},
-        fieldState: {error},
-      }) => {
-        // handle shrink on number input fields
-        if (type === 'number' && typeof value !== 'undefined') {
-          rest.InputLabelProps = rest.InputLabelProps || {}
-          rest.InputLabelProps.shrink = true
+      value={currentValue ?? ''}
+      onBlur={field.onBlur}
+      ref={ref}
+      onChange={(event) => {
+        let item: number | string = event.target.value
+        if (type === 'number' && item) {
+          item = Number(item)
         }
-
-        value = value?.[valueKey] ?? value // try fetch key value
-
-        return (
-          <TextField
-            {...rest}
-            name={name}
-            value={value ?? ''}
-            onBlur={onBlur}
-            onChange={(event) => {
-              let item: number | string = event.target.value
-              if (type === 'number' && item) {
-                item = Number(item)
-              }
-              onChange(item)
-              if (typeof rest.onChange === 'function') {
-                if (objectOnChange) {
-                  item = options.find((i) => i[valueKey] === item)
-                }
-                rest.onChange(item)
-              }
-            }}
-            select
-            required={required}
-            error={!!error}
-            helperText={
-              error
-                ? typeof customErrorFn === 'function'
-                  ? customErrorFn(error)
-                  : error.message
-                : rest.helperText
-            }
-            inputRef={ref}
-          >
-            {isNativeSelect && <option />}
-            {options.map((item: any) =>
-              createElement(
-                ChildComponent,
-                {
-                  key: `${name}_${item[valueKey]}`,
-                  value: item?.[valueKey] ?? item,
-                  disabled: item?.disabled ?? false,
-                },
-                item[labelKey]
-              )
-            )}
-          </TextField>
-        )
+        field.onChange(item)
+        if (typeof rest.onChange === 'function') {
+          if (objectOnChange) {
+            item = options.find((i) => i[valueKey] === item)
+          }
+          rest.onChange(item)
+        }
       }}
-    />
+      select
+      required={required}
+      error={!!error}
+      helperText={
+        error
+          ? typeof customErrorFn === 'function'
+            ? customErrorFn(error)
+            : error.message
+          : rest.helperText
+      }
+      inputRef={handleInputRef}
+    >
+      {isNativeSelect && <option />}
+      {options.map((item: any) =>
+        createElement(
+          ChildComponent,
+          {
+            key: `${name}_${item[valueKey]}`,
+            value: item?.[valueKey] ?? item,
+            disabled: item?.disabled ?? false,
+          },
+          item[labelKey]
+        )
+      )}
+    </TextField>
   )
-}
+}) as SelectElementComponent
+
+export default SelectElement
