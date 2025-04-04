e2304a7618a5482f1ba42c16121a021560963c28
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2025-04-04 07:43:17 +0100

    Fix: accessible issue for PasswordElement component visibility toggle

---
+++ apps/storybook/storybook-static/assets/ToggleButtonGroupElement.stories-Bc_FCVl_.js
@@ -0,0 +1,52 @@
+import{j as e}from"./jsx-runtime-BjG_zV1W.js";import{k as l,O as B,R as C,a as y}from"./chunk-D5ZWXAHU-Do8nhVH2.js";import{S as J}from"./Shared-BSjYdxAl.js";import"./index-Ca8FO3x4.js";import"./createTheme-pWRLLLPK.js";import"./index-P1EKaRb4.js";import"./v4-CQkTLCs1.js";import"./Stack-M2ksBymC.js";const _=l(e.jsx("path",{d:"M7 15v2h10v-2zm-4 6h18v-2H3zm0-8h18v-2H3zm4-6v2h10V7zM3 3v2h18V3z"}),"FormatAlignCenter"),k=l(e.jsx("path",{d:"M3 21h18v-2H3zm0-4h18v-2H3zm0-4h18v-2H3zm0-4h18V7H3zm0-6v2h18V3z"}),"FormatAlignJustify"),w=l(e.jsx("path",{d:"M15 15H3v2h12zm0-8H3v2h12zM3 13h18v-2H3zm0 8h18v-2H3zM3 3v2h18V3z"}),"FormatAlignLeft"),D=l(e.jsx("path",{d:"M3 21h18v-2H3zm6-4h12v-2H9zm-6-4h18v-2H3zm6-4h12V7H9zM3 3v2h18V3z"}),"FormatAlignRight"),ee={title:"Radio & Sliders & Switches/ToggleButtonGroupElement",component:B,decorators:[G]};function G(T){return e.jsxs(C,{defaultValues:{},onSuccess:y("submit"),children:[e.jsx(T,{}),e.jsx("br",{}),e.jsx(J,{})]})}const m=[{id:"first",label:"First"},{id:"second",label:"Second"},{id:"third",label:"Third"}],u=[{id:"left",label:e.jsx(w,{})},{id:"center",label:e.jsx(_,{})},{id:"right",label:e.jsx(D,{})},{id:"justify",label:e.jsx(k,{})}],s={args:{name:"basic",options:m}},r={args:{name:"exclusive",options:m,exclusive:!0}},t={args:{name:"exclusive",options:m,exclusive:!0,enforceAtLeastOneSelected:!0}},o={args:{name:"exclusive",options:u}},a={args:{name:"exclusive",options:u,label:"Please Select"}},n={args:{name:"exclusive",options:u,label:"Please Select",required:!0}},c={args:{name:"exclusive",options:u,label:"Please Select",required:!0,helperText:"Some helper text"}},i={args:{options:m,name:"fullWidth",label:"Full Width",fullWidth:!0}};var p,d,h;s.parameters={...s.parameters,docs:{...(p=s.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    name: 'basic',
+    options
+  }
+}`,...(h=(d=s.parameters)==null?void 0:d.docs)==null?void 0:h.source}}};var x,g,v;r.parameters={...r.parameters,docs:{...(x=r.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    name: 'exclusive',
+    options,
+    exclusive: true
+  }
+}`,...(v=(g=r.parameters)==null?void 0:g.docs)==null?void 0:v.source}}};var S,b,f;t.parameters={...t.parameters,docs:{...(S=t.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    name: 'exclusive',
+    options,
+    exclusive: true,
+    enforceAtLeastOneSelected: true
+  }
+}`,...(f=(b=t.parameters)==null?void 0:b.docs)==null?void 0:f.source}}};var z,H,W;o.parameters={...o.parameters,docs:{...(z=o.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    name: 'exclusive',
+    options: optionIcons
+  }
+}`,...(W=(H=o.parameters)==null?void 0:H.docs)==null?void 0:W.source}}};var j,F,I;a.parameters={...a.parameters,docs:{...(j=a.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    name: 'exclusive',
+    options: optionIcons,
+    label: 'Please Select'
+  }
+}`,...(I=(F=a.parameters)==null?void 0:F.docs)==null?void 0:I.source}}};var A,L,M;n.parameters={...n.parameters,docs:{...(A=n.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    name: 'exclusive',
+    options: optionIcons,
+    label: 'Please Select',
+    required: true
+  }
+}`,...(M=(L=n.parameters)==null?void 0:L.docs)==null?void 0:M.source}}};var V,E,R;c.parameters={...c.parameters,docs:{...(V=c.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    name: 'exclusive',
+    options: optionIcons,
+    label: 'Please Select',
+    required: true,
+    helperText: 'Some helper text'
+  }
+}`,...(R=(E=c.parameters)==null?void 0:E.docs)==null?void 0:R.source}}};var q,O,P;i.parameters={...i.parameters,docs:{...(q=i.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  args: {
+    options: options,
+    name: 'fullWidth',
+    label: 'Full Width',
+    fullWidth: true
+  }
+}`,...(P=(O=i.parameters)==null?void 0:O.docs)==null?void 0:P.source}}};const se=["Basic","Exclusive","EnforceOne","WithIcons","WithLabel","WithLabelRequired","WithCustomHelperText","FullWidth"];export{s as Basic,t as EnforceOne,r as Exclusive,i as FullWidth,c as WithCustomHelperText,o as WithIcons,a as WithLabel,n as WithLabelRequired,se as __namedExportsOrder,ee as default};
