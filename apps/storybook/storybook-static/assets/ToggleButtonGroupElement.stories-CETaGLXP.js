5ac45fd9904a43c500313638883285b2b1f16356
Jon Brook <jonbrook89@gmail.com>
2025-04-05 14:18:28 +0100

    add back sb static file changes

---
+++ apps/storybook/storybook-static/assets/ToggleButtonGroupElement.stories-CETaGLXP.js
@@ -0,0 +1,52 @@
+import{j as e}from"./jsx-runtime-BjG_zV1W.js";import{k as l,O as B,R as C,a as y}from"./chunk-D5ZWXAHU-dJp8butX.js";import{S as _}from"./Shared-rzVjZ017.js";import"./index-Ca8FO3x4.js";import"./createTheme-BZiKxHa3.js";import"./index-P1EKaRb4.js";import"./v4-CQkTLCs1.js";import"./Stack-DTJq516_.js";const k=l(e.jsx("path",{d:"M7 15v2h10v-2zm-4 6h18v-2H3zm0-8h18v-2H3zm4-6v2h10V7zM3 3v2h18V3z"})),w=l(e.jsx("path",{d:"M3 21h18v-2H3zm0-4h18v-2H3zm0-4h18v-2H3zm0-4h18V7H3zm0-6v2h18V3z"})),D=l(e.jsx("path",{d:"M15 15H3v2h12zm0-8H3v2h12zM3 13h18v-2H3zm0 8h18v-2H3zM3 3v2h18V3z"})),G=l(e.jsx("path",{d:"M3 21h18v-2H3zm6-4h12v-2H9zm-6-4h18v-2H3zm6-4h12V7H9zM3 3v2h18V3z"})),ee={title:"Radio & Sliders & Switches/ToggleButtonGroupElement",component:B,decorators:[J]};function J(T){return e.jsxs(C,{defaultValues:{},onSuccess:y("submit"),children:[e.jsx(T,{}),e.jsx("br",{}),e.jsx(_,{})]})}const m=[{id:"first",label:"First"},{id:"second",label:"Second"},{id:"third",label:"Third"}],u=[{id:"left",label:e.jsx(D,{})},{id:"center",label:e.jsx(k,{})},{id:"right",label:e.jsx(G,{})},{id:"justify",label:e.jsx(w,{})}],s={args:{name:"basic",options:m}},r={args:{name:"exclusive",options:m,exclusive:!0}},t={args:{name:"exclusive",options:m,exclusive:!0,enforceAtLeastOneSelected:!0}},o={args:{name:"exclusive",options:u}},a={args:{name:"exclusive",options:u,label:"Please Select"}},n={args:{name:"exclusive",options:u,label:"Please Select",required:!0}},c={args:{name:"exclusive",options:u,label:"Please Select",required:!0,helperText:"Some helper text"}},i={args:{options:m,name:"fullWidth",label:"Full Width",fullWidth:!0}};var p,d,h;s.parameters={...s.parameters,docs:{...(p=s.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    name: 'basic',
+    options
+  }
+}`,...(h=(d=s.parameters)==null?void 0:d.docs)==null?void 0:h.source}}};var x,v,g;r.parameters={...r.parameters,docs:{...(x=r.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    name: 'exclusive',
+    options,
+    exclusive: true
+  }
+}`,...(g=(v=r.parameters)==null?void 0:v.docs)==null?void 0:g.source}}};var S,b,z;t.parameters={...t.parameters,docs:{...(S=t.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    name: 'exclusive',
+    options,
+    exclusive: true,
+    enforceAtLeastOneSelected: true
+  }
+}`,...(z=(b=t.parameters)==null?void 0:b.docs)==null?void 0:z.source}}};var f,H,W;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    name: 'exclusive',
+    options: optionIcons
+  }
+}`,...(W=(H=o.parameters)==null?void 0:H.docs)==null?void 0:W.source}}};var j,I,F;a.parameters={...a.parameters,docs:{...(j=a.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    name: 'exclusive',
+    options: optionIcons,
+    label: 'Please Select'
+  }
+}`,...(F=(I=a.parameters)==null?void 0:I.docs)==null?void 0:F.source}}};var L,M,V;n.parameters={...n.parameters,docs:{...(L=n.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    name: 'exclusive',
+    options: optionIcons,
+    label: 'Please Select',
+    required: true
+  }
+}`,...(V=(M=n.parameters)==null?void 0:M.docs)==null?void 0:V.source}}};var E,q,A;c.parameters={...c.parameters,docs:{...(E=c.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    name: 'exclusive',
+    options: optionIcons,
+    label: 'Please Select',
+    required: true,
+    helperText: 'Some helper text'
+  }
+}`,...(A=(q=c.parameters)==null?void 0:q.docs)==null?void 0:A.source}}};var O,P,R;i.parameters={...i.parameters,docs:{...(O=i.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  args: {
+    options: options,
+    name: 'fullWidth',
+    label: 'Full Width',
+    fullWidth: true
+  }
+}`,...(R=(P=i.parameters)==null?void 0:P.docs)==null?void 0:R.source}}};const se=["Basic","Exclusive","EnforceOne","WithIcons","WithLabel","WithLabelRequired","WithCustomHelperText","FullWidth"];export{s as Basic,t as EnforceOne,r as Exclusive,i as FullWidth,c as WithCustomHelperText,o as WithIcons,a as WithLabel,n as WithLabelRequired,se as __namedExportsOrder,ee as default};
